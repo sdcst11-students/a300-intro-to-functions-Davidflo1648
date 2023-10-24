9fdfaef75e8b46a4a3b58d68fd62ad7bc6388d24
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-24 12:37:44 -0700

    task1 done

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
 def A():
+    print("You choose function A!")
 
 def B():
-
+    print("You choose function B!")
 
 def C():
+    print("You choose function C!")
+
+userInput = input("Choose between A, B, and C: ")
+
+if userInput == "A" or userInput == "a":
+    A()
+elif userInput == "B" or userInput == "b":
+    B()
+elif userInput == "C" or userInput == "c":
+    C()
